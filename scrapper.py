021fc907dc8a832b304cdfbd15bb78b28f1a04ba
andoniflores <andonibalsebre@gmail.com>
2023-12-07 04:55:33 -0500

    automation now login to bank account and added decryption for encrypted password used

--- scrapper.py
+++ scrapper.py
@@ -1,11 +1,32 @@
 from selenium import webdriver 
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 from dotenv import load_dotenv
 import os
 
+from cryptography.fernet import Fernet
+
+# Load environment variables from .env file
 load_dotenv()
+
+# Load the encryption key
+with open("encryption_key.key", "rb") as key_file:
+    key = key_file.read()
+
+# Get the encrypted password from .env
+encrypted_password = os.getenv("ENCRYPTED_PASSWORD")
+
+# Decrypt the password
+cipher_suite = Fernet(key)
+decrypted_password = cipher_suite.decrypt(encrypted_password.encode()).decode()
+
+# Now, you can use the decrypted password in your code
+print(decrypted_password)
+
 BANK_WEBSITE_URL=os.getenv('BANK_WEBSITE_URL')
+USERNAME=os.getenv('USERNAME')
+PASSWORD=decrypted_password
 
 options = webdriver.FirefoxOptions()
 options.add_argument("--enable-javascript")
@@ -15,8 +36,16 @@ driver.get(BANK_WEBSITE_URL)
 
 time.sleep(5)
 
+username = driver.find_element(By.ID, 'principal_name')
+password = driver.find_element(By.ID, 'password')
+username.send_keys(USERNAME)
+password.send_keys(PASSWORD)
+driver.find_element(By.ID, 'challenge-commit').click()
+
 html = driver.page_source
 soup = BeautifulSoup(html)
 print(soup.prettify())
+
+time.sleep(10)
 
 driver.quit()
