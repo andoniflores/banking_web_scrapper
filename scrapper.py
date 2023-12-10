ffcc949336e53ded1474cd6121b9674d9bef27fb
andoniflores <andonibalsebre@gmail.com>
2023-12-10 04:38:21 -0500

    data scraped into dictionary

--- scrapper.py
+++ scrapper.py
@@ -3,8 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from bs4 import BeautifulSoup
 import time
-import dotenv
-from dotenv import load_dotenv
+from dotenv import load_dotenv, set_key, find_dotenv
 import os
 
 from cryptography.fernet import Fernet
@@ -27,7 +26,7 @@ decrypted_password = cipher_suite.decrypt(encrypted_password.encode()).decode()
 # print(decrypted_password)
 
 BANK_WEBSITE_URL=os.getenv('BANK_WEBSITE_URL')
-USERNAME=os.getenv('USERNAME')
+BANK_USERNAME=os.getenv('BANK_USERNAME')
 PASSWORD=decrypted_password
 REMEMBER_DEVICE_COOKIE=os.getenv('REMEMBER_DEVICE_COOKIE')
 
@@ -45,7 +44,7 @@ driver.add_cookie({'name': 'did', 'value': REMEMBER_DEVICE_COOKIE})
 # Fill login fields
 username = driver.find_element(By.ID, 'principal_name')
 password = driver.find_element(By.ID, 'password')
-username.send_keys(USERNAME)
+username.send_keys(BANK_USERNAME)
 password.send_keys(PASSWORD)
 driver.find_element(By.ID, 'challenge-commit').click()
 
@@ -65,20 +64,34 @@ try:
 
     # Save 'did' cookie to skip 2FA next time
     remember_device_cookie = driver.get_cookie('did')
-    print(remember_device_cookie)
     os.environ['REMEMBER_DEVICE_COOKIE'] = remember_device_cookie['value'] 
-    dotenv.set_key(dotenv.find_dotenv(), 'REMEMBER_DEVICE_COOKIE', os.environ['REMEMBER_DEVICE_COOKIE'])
+    set_key(find_dotenv(), 'REMEMBER_DEVICE_COOKIE', os.environ['REMEMBER_DEVICE_COOKIE'])
 except:
     print("[INFO] Couldn't find remember device element, device is trusted")
 
 # You are succesfully logged in to banking site!
 
 # Find and click account summary
+wait.until(lambda d: driver.find_element(By.XPATH, "//div[contains(@id, 'account_summary')]//a[contains(@href, '/account/')]").is_displayed())
 driver.find_element(By.XPATH, "//div[contains(@id, 'account_summary')]//a[contains(@href, '/account/')]").click()
 
 html = driver.page_source
 soup = BeautifulSoup(html, 'html5lib')
-# print(soup.prettify())
+
+table = soup.find('table', attrs = {'id':'posted-table'})
+bank_data = []
+for row in table.findAll('tr'):
+    clss = row.attrs.get('class')
+    if clss is not None and "searchable" in clss:
+        data = {}
+        data['date'] = row.find('td', title=True)['title']
+        data['description'] = row.find('td', class_='cW cVT cUT').contents[0].text.strip()
+        data['amount_spent'] = row.find('td', class_='cR cVT').text.strip()
+        data['amount_received'] = row.find('td', class_='cR cVT').find_next_sibling().text.strip()
+        data['balance'] = row.find('td', class_='cR cVT cUT').text.strip()
+        bank_data.append(data)
+
+print(bank_data)
 
 time.sleep(10)
 
